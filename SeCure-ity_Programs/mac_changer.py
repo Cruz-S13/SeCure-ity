f9e520d90cd0de2a7df5eaef74e01887f2f0371e
John Cruz <77587169+Cruz-S13@users.noreply.github.com>
2021-02-19 01:47:58 -0800

    Update mac_changer.py
    updated the code to run correctly.

--- SeCure-ity_Programs/mac_changer.py
+++ SeCure-ity_Programs/mac_changer.py
@@ -6,27 +6,14 @@
 #imports the subprocess module that allows you to use bash commands when called in the program.
 import subprocess
 
-user_conf = raw_input("This program changes the MAC address of a specified target. Do you want to proceed (Yes/no)? ")
+#Receive input from the user on which network interface to use and change the MAC address
+interface = input("interface > ")
+new_mac = input("new MAC > ")
 
-if user_conf = "no"
-    break;
-    else
-        t_interface = raw_input("interface > ")
-        n_mac = raw_input("new MAC > ")
-        print("[+] Changing MAC address for " + t_interface + " to " + n_mac)
-        subprocess.call(["ifconfig", t_interface, "down"])
-        subprocess.call(["ifconfig", t_interface, "hw", "ether", n_mac])
-        subprocess.call(["ifconfig", t_interface, "up"])
-    else
-        break;
+#Feedback confirming what the user input.
+print("[+] Changing MAC address for " + t_interface + " to " + n_mac)
 
-
-#Uncomment the following to revert the last change you made to your MAC address.
-# print("You are reverting the last change you made to your MAC Address: "
-#       "\nNetwork Interface: " + net_intface +
-#       "\nNew MAC Address: " + orig_intface +
-#       "\nOld MAC Address: " + new_intface)
-#
-# subprocess.call("ifconfig eth0 down", shell=True) #Takes interface back down
-# subprocess.call("ifconfig eth0 hw ether 08:00:27:ec:30:a5", shell=True) #Changes the mac address of eth0 to the original
-# subprocess.call("ifconfig eth0 up", shell=True) #Brings eth0 interface active
+#Calling the subprocess module to perform the bash command: ifconfig <args>
+subprocess.call(["ifconfig", t_interface, "down"])
+subprocess.call(["ifconfig", t_interface, "hw", "ether", n_mac])
+subprocess.call(["ifconfig", t_interface, "up"])
