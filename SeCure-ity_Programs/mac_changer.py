1e7e7518fc8242745e363d410a72ac9b26e884ae
John Cruz <77587169+Cruz-S13@users.noreply.github.com>
2021-02-19 11:24:03 -0800

    Update mac_changer.py
    added if/else statements and options for user input.

--- SeCure-ity_Programs/mac_changer.py
+++ SeCure-ity_Programs/mac_changer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 #Kali Machine OG MAC_Add: 08:00:27:ec:30:a5
 #Change to address:       00:11:22:33:44:55
@@ -6,14 +6,16 @@
 #imports the subprocess module that allows you to use bash commands when called in the program.
 import subprocess
 
-#Receive input from the user on which network interface to use and change the MAC address
-interface = input("interface > ")
-new_mac = input("new MAC > ")
+user_conf = input("This program changes the MAC address of a specified target. Do you want to proceed (Yes/no)? ")
 
-#Feedback confirming what the user input.
-print("[+] Changing MAC address for " + t_interface + " to " + n_mac)
-
-#Calling the subprocess module to perform the bash command: ifconfig <args>
-subprocess.call(["ifconfig", t_interface, "down"])
-subprocess.call(["ifconfig", t_interface, "hw", "ether", n_mac])
-subprocess.call(["ifconfig", t_interface, "up"])
+if user_conf == "no" or user_conf == "No" or user_conf == "NO" or user_conf == "":
+    print("This operation will now end.")
+elif user_conf == "Yes" or user_conf == "yes" or user_conf == "YES":
+    t_interface = input("interface > ")
+    n_mac = input("new MAC > ")
+    print("[+] Changing MAC address for " + t_interface + " to " + n_mac)
+    subprocess.call(["ifconfig", t_interface, "down"])
+    subprocess.call(["ifconfig", t_interface, "hw", "ether", n_mac])
+    subprocess.call(["ifconfig", t_interface, "up"])
+else:
+    print("Either you did not choose a valid selection, interface, or MAC address. This program will now end.")
